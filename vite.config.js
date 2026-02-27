32fa74a549bfdf5d1b3542789628b8a476367666
杨宗深 <14630928+yang-zongshen@user.noreply.gitee.com>
2026-02-27 12:33:47 +0800

    降级兼容修复

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    legacy({
+      // 面向市面上绝大多数的老旧浏览器，完美覆盖 360 的各种奇怪内核版本
+      targets: ['defaults', 'not IE 11', 'chrome >= 49'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
   ],
   resolve: {
     alias: {
