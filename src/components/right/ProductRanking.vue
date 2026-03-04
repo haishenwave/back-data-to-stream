3689f48021739d02bb13053e3b37134a193df9e1
杨宗深 <14630928+yang-zongshen@user.noreply.gitee.com>
2026-03-04 21:10:28 +0800

    数据适配

--- src/components/right/ProductRanking.vue
+++ src/components/right/ProductRanking.vue
@@ -30,7 +30,7 @@
 // 完全复刻 PDF 中的四大商品精确实参
 const products = [
   { name: '火山南瓜', sales: '240', amount: '7,176' },
-  { name: '小米', sales: '642', amount: '6,345.9' },
+  { name: '小米', sales: '642', amount: '6,355.8' },
   { name: '火山西红柿', sales: '93', amount: '4,640.7' },
   { name: '文旅路线', sales: '5', amount: '495' },
 ];
